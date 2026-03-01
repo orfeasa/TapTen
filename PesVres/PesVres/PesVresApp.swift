f815abc91644283e6424f3c67ce968c8b65401e9
Orfeas Antoniou <orfeas@orfeasa.com>
2026-03-01 18:45:58 +0000

    xcode init

---
+++ PesVres/PesVres/PesVresApp.swift
@@ -0,0 +1,17 @@
+//
+//  PesVresApp.swift
+//  PesVres
+//
+//  Created by Orfeas Antoniou on 01/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct PesVresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PesVres/PesVresTests/PesVresTests.swift
@@ -0,0 +1,17 @@
+//
+//  PesVresTests.swift
+//  PesVresTests
+//
+//  Created by Orfeas Antoniou on 01/03/2026.
+//
+
+import Testing
+@testable import PesVres
+
+struct PesVresTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
